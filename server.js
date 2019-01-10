fc1834362d546fdf13226f5a6704e1301f781318
Amey Deshpande <ameydeshpande30@gmail.com>
2019-01-10 22:02:51 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+var app = express();
+
+//list of all Notification
+app.get('/listNotify', (req, res) => {
+  t = new Date();
+  data =
+    [
+      data1 = {
+       'id' : 1,
+       'link' : 'https://homepages.cae.wisc.edu/~ece533/images/tulips.png',
+       'ts' : t,
+       'title': 'Test-1',
+       'body': 'Want to learn more? I have free mini-courses over on my website. These are 30 minute videos that cover new and interesting topics.',
+   },
+   data2 = {
+       'id': 2,
+       'title': 'Test-2',
+       'ts': t,
+       'body': 'Want to learn more? I have free mini-courses over on my website. These are 30 minute videos that cover new and interesting topics.',
+   },
+   data3 = {
+       'id': 3,
+       'title': 'Test-3',
+       'ts': t,
+       'body': 'Want to learn more? I have free mini-courses over on my website. These are 30 minute videos that cover new and interesting topics.',
+   }
+    ]
+    res.send(data, () => {
+      console.log('.... Starting Web Server .....');
+    })
+});
+
+
+
+app.listen(3000);
