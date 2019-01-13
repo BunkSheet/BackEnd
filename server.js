3b8129e09422f5f9de74662ced46da70e4d76c2c
Amey Deshpande <ameydeshpande30@gmail.com>
2019-01-14 01:24:49 +0530

    Setting Up Server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+port = process.env.PORT || 3000 ;
 var app = express();
 
 require('./CraftyClown/main')(app);
 require('./BlazeHunter/main')(app);
 require('./Nitin/main')(app);
 
-app.listen(3000);
+app.listen(port);
