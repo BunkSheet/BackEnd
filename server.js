ac6369e3db8279b1509d7a4fd9902a443cb423b9
Amey Deshpande <ameydeshpande30@gmail.com>
2019-01-14 01:36:11 +0530

    Sample Page

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 port = process.env.PORT || 3000 ;
 var app = express();
-
+var hbs  = require('hbs');
+app.set('view engine', 'hbs');
+app.get('/', function (req, res) {
+    res.render('maintenance');
+});
 require('./CraftyClown/main')(app);
 require('./BlazeHunter/main')(app);
 require('./Nitin/main')(app);
